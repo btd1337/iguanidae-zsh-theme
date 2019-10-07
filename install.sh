244090f56a8ee91dd372da52d638a1c1c2d8cec9
btd1337 <helder.bertoldo@ice.ufjf.br>
2019-10-07 11:26:58 -0300

    update install message

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.oh-my-zsh/custom/themes/
 cp iguanidae.zsh-theme ~/.oh-my-zsh/custom/themes/
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="iguanidae"/g' ~/.zshrc
-echo "Now reopen your terminal..."
+sed -i 's/ZSH_THEME="*"/ZSH_THEME="iguanidae"/g' ~/.zshrc
+echo "Now restart your user session or your system..."
